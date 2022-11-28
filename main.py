36d2f8d08d209ad0deb16dc0c567f6f96f16b5e9
EsharkyTheGreat <eshwars1210@gmail.com>
2022-11-28 22:19:16 +0530

    More Changes

--- main.py
+++ main.py
@@ -22,44 +22,49 @@ login_data = {
 }
 res = s.post(LOGIN_URL, data=login_data)
 
-notice_board_req = s.get(NOTICE_BOARD)
-notice_board_soup = bs4.BeautifulSoup(notice_board_req.text, 'html.parser')
-all_table_elements = notice_board_soup.find_all(
-    'td', attrs={'class': 'topic-name'})
+for i in range(10):
+    notice_board_req = s.get(NOTICE_BOARD+f'?page={i+1}')
+    notice_board_soup = bs4.BeautifulSoup(notice_board_req.text, 'html.parser')
+    all_table_elements = notice_board_soup.find_all(
+        'td', attrs={'class': 'topic-name'})
 
 
-for table_ele in all_table_elements:
-    link_suffix = table_ele.a['href']
-    if 'shortlist' in link_suffix and '-ft' in link_suffix and 'interview' in link_suffix:
-        ROLL_NOS = []
-        EMAIL_IDS = []
-        company_name = link_suffix.split("/")[-2]
-        post_req = s.get(
-            "https://www.placement.iitbhu.ac.in/"+link_suffix).text
-        contentSoup = bs4.BeautifulSoup(post_req, 'html.parser')
-        content = contentSoup.find(
-            'td', attrs={'class': 'post-content'})
-        rolls = re.findall('\d{8}', content.text)
-        print(company_name)
-        if len(rolls) == 0:
-            # Must Be CSV or ODS
-            link = contentSoup.find("div", attrs={"class": "attachments"})
-            if len(link.text) > 0:
-                linkObject = link.a["href"]
-                if "csv" in linkObject:
-                    # Parse CSV
-                    pass
-                elif "ods" in linkObject:
-                    with s.get("https://www.placement.iitbhu.ac.in/" + linkObject) as r:
-                        with open('./data.ods', "wb") as f:
-                            f.write(r.content)
-                    esharky.convertODStoCSV("./data.ods")
-                    with open("./data.csv","r") as f:
-                        print(f.read())
-        else:
-            # Check for Name and Mail and write
-            # content_split = content.split(" ")
-            # for ele in content_split:
-            #     if "che19" in ele:
-            #         pass
-            pass
+    for table_ele in all_table_elements:
+        link_suffix = table_ele.a['href']
+        if 'shortlist' in link_suffix and '-ft' in link_suffix and 'interview' in link_suffix:
+            ROLL_NOS = []
+            EMAIL_IDS = []
+            company_name = link_suffix.split("/")[-2]
+            post_req = s.get(
+                "https://www.placement.iitbhu.ac.in/"+link_suffix).text
+            contentSoup = bs4.BeautifulSoup(post_req, 'html.parser')
+            content = contentSoup.find(
+                'td', attrs={'class': 'post-content'})
+            rolls = re.findall('\d{8}', content.text)
+            print(company_name)
+            if len(rolls) == 0:
+                # Must Be CSV or ODS
+                link = contentSoup.find("div", attrs={"class": "attachments"})
+                if len(link.text) > 0:
+                    linkObject = link.a["href"]
+                    if "csv" in linkObject:
+                        print("CSV")
+                        # Parse CSV
+                        pass
+                    elif "ods" in linkObject:
+                        with s.get("https://www.placement.iitbhu.ac.in/" + linkObject) as r:
+                            with open('./data.ods', "wb") as f:
+                                f.write(r.content)
+                        esharky.convertODStoCSV("./data.ods")
+                        with open("./data.csv","r") as f:
+                            print(f.read())
+                    elif "xlsx" in linkObject:
+                        # Parse xlsx
+                        print("XLSX")
+            else:
+                # Check for Name and Mail and write
+                content_split = content.text.split(" ")
+                for ele in content_split:
+                    if "che19" in ele:
+                        pass
+                pass
